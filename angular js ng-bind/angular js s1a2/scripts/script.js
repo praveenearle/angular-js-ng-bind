ad98ef69011bf7186ee561ea24e440753c2af2da
praveenearle <praveen.earle@gmail.com>
2016-09-27 00:08:44 +0530

    ng-bind
    ng-bind

---
+++ angular js ng-bind/angular js s1a2/scripts/script.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+
+    $scope.message = "This is Ng-Bind directive";
+    
+
+         
+    }); 
+
